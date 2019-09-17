f0df9ccc036c80309253594aeb73f0ae43a14f3a
Eugeny Kravchenko <e.kravchenko.end@gmail.com>
2019-09-17 21:00:48 +0300

    updated docker image and repo name for restapi

--- packAndPublishContainers.sh
+++ packAndPublishContainers.sh
@@ -1,5 +1,5 @@
 docker build -t ekravchenko/notes-ui:latest ../notes-ui
 docker push ekravchenko/notes-ui:latest
 
-docker build -t ekravchenko/restapi:latest ../noderestapi
-docker push ekravchenko/restapi:latest
+docker build -t ekravchenko/notes-rest-api:latest ../notes-rest-api
+docker push ekravchenko/notes-rest-api:latest
